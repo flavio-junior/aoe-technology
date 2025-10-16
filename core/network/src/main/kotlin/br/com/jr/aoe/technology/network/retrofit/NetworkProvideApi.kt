403c2ff43d675ad17c3cd127c43da9fc67200865
Flávio Júnior <59364674+flavio-junior@users.noreply.github.com>
2025-10-16 14:47:01 -0300

    feat: add BuildConfig in Network module

--- core/network/src/main/kotlin/br/com/jr/aoe/technology/network/retrofit/NetworkProvideApi.kt
+++ core/network/src/main/kotlin/br/com/jr/aoe/technology/network/retrofit/NetworkProvideApi.kt
@@ -1,5 +1,6 @@
 package br.com.jr.aoe.technology.network.retrofit
 
+import br.com.jr.aoe.technology.network.BuildConfig
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -10,7 +11,7 @@ fun provideRetrofit(
     gsonConverterFactory: GsonConverterFactory
 ): Retrofit {
     return Retrofit.Builder()
-        .baseUrl("BuildConfig.URL_API")
+        .baseUrl(BuildConfig.URL_API)
         .client(okHttpClient)
         .addConverterFactory(gsonConverterFactory)
         .build()
